ce3b0aaaaed9f55d5a5357e44f7ae3ce71033ca3
Enrique <enriquep36@hotmail.com>
2018-12-10 21:48:37 -0500

    entrada de menu

---
+++ components/header.vue
@@ -0,0 +1,6 @@
+<template lang="html">
+<div class="">
+  <p>Hola</p>
+
+</div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,16 @@
 <template>
-<nuxt/>
+  <div class="">
+    <Header/>
+    <nuxt/>
+  </div>
 </template>
 
 <script>
+  import Header from '@/components/Header'
   export default {
-
+    components: {
+      Header
+    }
   }
+
 </script>
